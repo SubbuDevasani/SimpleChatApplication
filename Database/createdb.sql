fa0bcf3d668791b6732a59c0ea6f635ed6808fbf
ManiDevasani <mani.devasani@gmail.com>
2020-01-14 13:12:31 +0000

    First commit

---
+++ Database/createdb.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE chatapp;
+CREATE USER manidevasani WITH PASSWORD 'Manikanta';
+GRANT ALL PRIVILEGES ON DATABASE "chatapp" to manidevasani;
+
--- scripts/app_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo systemctl daemon-reload
-sudo systemctl start gunicorn
-sudo systemctl enable gunicorn
-sudo service gunicorn restart
-sudo service nginx restart 
-
