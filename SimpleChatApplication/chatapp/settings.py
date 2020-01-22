9b45e9a90c9915640114a4560cfd83d912e6bc96
Mani Devasani <56950679+ManiDevasani@users.noreply.github.com>
2020-01-22 18:19:14 +0530

    Update settings.py

--- SimpleChatApplication/chatapp/settings.py
+++ SimpleChatApplication/chatapp/settings.py
@@ -139,7 +139,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
  os.path.join(BASE_DIR, 'static'),
 )
-STATIC_ROOT = os.path.join(BASE_DIR, 'django')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
